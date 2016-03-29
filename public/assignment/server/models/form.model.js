8bd6e90acd6a65381116255020d8c7902aace6d3
Sneha Lakshmisha <lsneha@ccs.neu.edu>
2016-03-29 15:50:30 -0400

    form model

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -22,6 +22,8 @@ module.exports = function(app) {
         findFormById: findFormById
     };
 
+    return formApi;
+
     function setCurrentForm(form) {
         $scope.currentForm = form;
     }
@@ -31,56 +33,124 @@ module.exports = function(app) {
     }
 
     function findAllForms() {
-        return mock;
+        var deferred = q.defer();
+
+        FormModel.findAllForms(function(err, forms){
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(forms);
+            }
+        });
+
+        return deferred.promise;
     }
 
     function createForm(form)
     {
-        form._id = "ID_"+(new Date()).getTime();
+        /*form._id = "ID_"+(new Date()).getTime();
         mock.push(form);
-        return form;
+        return form;*/
+        var deferred = q.defer();
+        console.log("Create a form");
+
+        FormModel.createForm(user, function(err, form) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                console.log("Form>");
+                console.log(form);
+                deferred.resolve(form);
+            }
+        });
+
+        return deferred.promise;
     }
 
     function deleteForm(formId)
     {
-        for(var f in mock) {
+        /*for(var f in mock) {
             if(formId === mock[f]._id) {
                 var index = model.Forms.indexOf(mock[f]);
                 model.Forms.splice(index, 1);
             }
         }
-        return model.Forms[f];
+        return model.Forms[f];*/
+        var deferred = q.defer();
+
+        FormModel.deleteForm(formId, function(err, status) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(status);
+            }
+        });
+
+        return deferred.promise;
     }
 
-    function updateForm(form, update)
+    function updateForm(formId, form)
     {
-        for(var f in mock) {
+        /*for(var f in mock) {
             if(mock[f]._id === form._id){
                 mock[f] = update;
                 return mock[f];
             }
         }
-        return null;
+        return null;*/
+        var deferred = q.defer();
+
+        form.delete("formId");
+
+        FormModel.updateForm({formId: formId}, {$set: form}, function(err, form) {
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(form);
+            }
+        });
+
+        return deferred.promise;
     }
 
     function findFormByTitle(title) {
-        for (var f in mock) {
+        /*for (var f in mock) {
             if (mock[f].title) {
                 return mock[f];
             }
         }
-        return null;
+        return null;*/
+        var deferred = q.defer();
+
+        FormModel.findFormByTitle(title, function(err, form){
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(form);
+            }
+        });
+
+        return deferred.promise;
     }
 
     function findFormById(formId) {
-        for (var f in mock) {
+        /*for (var f in mock) {
             if (mock[f]._id === formId) {
                 return mock[f];
             }
         }
-        return null;
-    }
+        return null;*/
+        var deferred = q.defer();
 
-    return formApi;
+        FormModel.findFormById(formId, function(err, form){
+            if(err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(form);
+            }
+        });
+
+        return deferred.promise;
+    }
 
 }
