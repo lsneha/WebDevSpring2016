8b4bc8eb7aba760eaf6be41f5e7c999372abbb4f
Sneha Lakshmisha <lsneha@ccs.neu.edu>
2016-03-29 17:48:28 -0400

    client side programming assignment copy

---
+++ public/ClientSide/views/home/home.controller.js
@@ -0,0 +1,16 @@
+/**
+ * Created by sneha_000 on 2/15/2016.
+ */
+(function(){
+    "use strict";
+    angular
+        .module("FormBuilderApp")
+        .controller("HomeController", HomeController);
+
+    function HomeController($scope, UserService)
+    {
+        $scope.isUserLoggedIn = false;
+        /*user lookup*/
+    }
+
+})();
